715b3b0e1d593739ce25e004f146feaa5a41664c
Narcis Palomeras <narcis.palomeras@udg.edu>
2025-09-08 09:19:28 +0200

    Training configuration

--- scripts/train.py
+++ scripts/train.py
@@ -4,5 +4,5 @@ from botifarra.dqn_botifarra import DQNBotifarra
 agent = DQNBotifarra()
 
 env = BotifarraEnv()
-agent.training(env, 1000, log_every=100)
-agent.save_weights("./botifarra_v2_10k_dqn")
+agent.training(env, 1000000, save_every=10000, log_every=2000)
+agent.save_weights("./botifarra_1M_dqn")
